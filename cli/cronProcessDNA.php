365208d6becd262b41568ce032e964bc8a77bfab
Glenn Eggeton <geggleton@mobials.com>
2018-02-18 12:02:34 -0500

    Update

--- cli/cronProcessDNA.php
+++ cli/cronProcessDNA.php
@@ -10,7 +10,7 @@ $container = $app->getContainer();
 
 $pdo = $container->get(PDO::class);
 
-$statement = $pdo->prepare('update kitties set `gene_hex` = ?, `gene_bin` = ?, gene_kai = ? where id = ?');
+$statement = $pdo->prepare('update kitties set `genes_hex` = ?, `genes_bin` = ?, genes_kai = ? where id = ?');
 
 $date = date('Ymd', strtotime('yesterday'));
 $file = "/tmp/{$date}.csv";
@@ -18,17 +18,27 @@ print $file;
 
 $fh = fopen($file, 'r');
 
+$row = 0;
 while ($line = fgetcsv($fh))
 {
+    if ($row == 0) {
+        $row++;
+        continue;
+    }
+
     $id = $line[0];
     $genes_hex = $line[6];
     $genes_bin = $line[7];
     $genes_kai = $line[8];
 
-    $statement->execute([
+    if (!$statement->execute([
         $genes_hex,
         $genes_bin,
         $genes_kai,
         $id
-    ]);
+    ])) {
+        print "Error\n\n";
+        print $statement->errorInfo();
+        print_r($statement->errorInfo());
+    }
 }
