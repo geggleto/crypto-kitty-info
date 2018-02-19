55483cfda2e2a2f3c4d20976efab478baf2dc6ed
Glenn Eggeton <geggleton@mobials.com>
2018-02-18 21:38:51 -0500

    Update

--- public/index.php
+++ public/index.php
@@ -8,5 +8,19 @@ include_once __DIR__ . '/../vendor/autoload.php';
 $app = new KittyApp();
 
 $app->get('/dna/body/{code}', [DnaController::class, 'searchBody']);
+$app->get('/dna/body/color/{code}', [DnaController::class, 'searchBodyColor']);
+
+$app->get('/dna/pattern/{code}', [DnaController::class, 'searchPattern']);
+$app->get('/dna/pattern/color/{code}', [DnaController::class, 'searchPatternColor']);
+$app->get('/dna/eye/color/{code}', [DnaController::class, 'searchEyeColor']);
+$app->get('/dna/eye/type/{code}', [DnaController::class, 'searchEyeType']);
+
+$app->get('/dna/secondary/color/{code}', [DnaController::class, 'searchSecondaryColor']);
+
+$app->get('/dna/wild/{code}', [DnaController::class, 'searchWild']);
+
+$app->get('/dna/mouth/{code}', [DnaController::class, 'searchMouth']);
+
+
 
 $app->run();
--- src/Http/DnaController.php
+++ src/Http/DnaController.php
@@ -28,4 +28,68 @@ class DnaController
 
         return $response->withJson($this->kittyService->findBody($code));
     }
+
+    public function searchPattern($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findPattern($code));
+    }
+
+    public function searchEyeColor($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findEyeColor($code));
+    }
+
+    public function searchEyeType($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findEyeType($code));
+    }
+
+    public function searchBodyColor($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findBodyColor($code));
+    }
+
+    public function searchPatternColor($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findPatternColour($code));
+    }
+
+    public function searchSecondaryColour($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findSecondaryColour($code));
+    }
+
+    public function searchWild($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findWild($code));
+    }
+
+    public function searchMouth($code, Request $request, Response $response) {
+        if (strlen($code) !== 4) {
+            return $response->withStatus(400);
+        }
+
+        return $response->withJson($this->kittyService->findMouth($code));
+    }
 }
