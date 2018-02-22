0b3fe7ec7d98f1c9fb91b26c6f3ec179f6560319
Glenn Eggeton <geggleton@mobials.com>
2018-02-21 22:47:20 -0500

    Fix Di definition problem

--- config/di-container.php
+++ config/di-container.php
@@ -29,8 +29,8 @@ return [
         ]);
 
         // Instantiate and add Slim specific extension
-        $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
-        $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+        $basePath = rtrim(str_ireplace('index.php', '', $c->get('request')->getUri()->getBasePath()), '/');
+        $view->addExtension(new \Slim\Views\TwigExtension($c->get('router'), $basePath));
 
         return $view;
     }
