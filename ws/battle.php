2d749cbf5da763f8c98a0980f427e060e4b877ed
Glenn Eggeton <geggleton@mobials.com>
2018-02-28 23:40:56 -0500

    Update

--- ws/battle.php
+++ ws/battle.php
@@ -5,6 +5,6 @@ use Kitty\WebSockets\CryptoBattle;
 include_once __DIR__ . '/../vendor/autoload.php';
 
 // Run the server application through the WebSocket protocol on port 8080
-$app = new Ratchet\App('localhost', 8080);
+$app = new Ratchet\App('localhost', 8090);
 $app->route('/battle', new CryptoBattle());
 $app->run();
