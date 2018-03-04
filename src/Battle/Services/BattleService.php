751f04ba4f5baa5765619c9201074153cac8eeb6
Glenn Eggeton <geggleton@mobials.com>
2018-03-03 22:12:14 -0500

    Update

--- src/Battle/Services/BattleService.php
+++ src/Battle/Services/BattleService.php
@@ -55,14 +55,14 @@ class BattleService
         $attacker = null;
         $defender = null;
 
-        if ($battle->getKitty1()->getId() === $actionTaken->getKittyId())
+        if ($battle->getKitty1()->getId() == $actionTaken->getKittyId())
         {
-            var_dump("Player 1 Attacking {$actionTaken->getAddress()}");
+            var_dump("Player 1 Attacking {$actionTaken->getKittyId()}");
             //Player 1 Attacker
             $attacker = $battle->getKitty1();
             $defender = $battle->getKitty2();
         } else {
-            var_dump("Player 2 Attacking {$actionTaken->getAddress()}");
+            var_dump("Player 2 Attacking {$actionTaken->getKittyId()}");
             //Player 2 Attacker
             $attacker = $battle->getKitty2();
             $defender = $battle->getKitty1();
