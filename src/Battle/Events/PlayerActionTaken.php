7789a560f823e65b8fc7353ffa779d5b29650320
Glenn Eggeton <geggleton@mobials.com>
2018-03-03 21:14:42 -0500

    Update

--- src/Battle/Events/PlayerActionTaken.php
+++ src/Battle/Events/PlayerActionTaken.php
@@ -5,13 +5,21 @@ namespace Kitty\Battle\Events;
 
 
 use Kitty\Battle\Entities\PlayerConnection;
+use Symfony\Component\EventDispatcher\Event;
 
-class PlayerActionTaken
+class PlayerActionTaken extends Event
 {
     public const EVENT_ROUTING_KEY = 'player.take.turn';
+    /**
+     * @var PlayerConnection
+     */
+    private $connection;
+    private $skill;
+    private $battleId;
 
     public function __construct(PlayerConnection $connection, $skill, $battleId)
     {
+
         $this->connection = $connection;
         $this->skill = $skill;
         $this->battleId = $battleId;
