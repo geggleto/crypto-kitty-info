c3bac3b8dd79399c0bbde3973461b532f2309c25
Glenn Eggeton <geggleton@mobials.com>
2018-03-06 15:46:27 -0500

    Fix

--- ws/Battle/findKittyConsumer.php
+++ ws/Battle/findKittyConsumer.php
@@ -8,7 +8,7 @@ use React\EventLoop\Factory;
 
 include_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = new \Dotenv\Dotenv(__DIR__.'/../');
+$dotenv = new \Dotenv\Dotenv(__DIR__.'/../../');
 $dotenv->load();
 
 $pdo = new PDO('mysql:host='.getenv('MYSQL_HOST').';dbname='.getenv('MYSQL_DATABASE'), getenv('MYSQL_USERNAME'), getenv('MYSQL_PASSWORD'));
