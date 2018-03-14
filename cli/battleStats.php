03b72709296bc1ee32e103c9396643406c8ad236
Glenn Eggeton <geggleton@mobials.com>
2018-03-13 21:29:43 -0400

    Update

---
+++ cli/battleStats.php
@@ -0,0 +1,26 @@
+<?php
+
+use Bunny\Client;
+use Kitty\Battle\Consumers\BattleStatsConsumer;
+use Kitty\Battle\Producer\BattleUsageProducer;
+
+set_time_limit(0);
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+$log = new Monolog\Logger('crypto');
+$log->pushHandler(new StreamHandler(__DIR__.'/../logs/battlestats.log', Logger::DEBUG));
+
+
+$bunny = new Client(
+    [
+        'host'      => 'localhost',
+        'vhost'     => '/',    // The default vhost is /
+        'user'      => getenv('RABBIT_USER'), // The default user is guest
+        'password'  => getenv('RABBIT_PASSWORD'), // The default password is guest
+    ]
+);
+
+$channel = $bunny->channel();
+
+$channel->run(new BattleStatsConsumer($log), BattleUsageProducer::FETCH_QUEUE);
