6731ff02f5704e6d8dacb6bdcadd0d26c8190ff3
Glenn Eggeton <geggleton@mobials.com>
2018-04-15 17:05:11 -0400

    Update

--- public/index.php
+++ public/index.php
@@ -1,55 +1,16 @@
 <?php
 
-use Kitty\Http\BattleZone\BattlePage;
-use Kitty\Http\BattleZone\Profile;
+use Kitty\Http\Authorization;
 use Kitty\Http\CorsBullshit;
-use Kitty\Http\DnaController;
-use Kitty\Http\GetCattributesForKitty;
-use Kitty\Http\HomePage;
-use Kitty\Http\MyProfile;
 use Kitty\Http\ProfileController;
-use Kitty\Http\Search\SearchController;
 use Kitty\Http\SearchDnaController;
-use Kitty\Http\SearchPage;
 use Kitty\KittyApp;
-use Kitty\Security\Authorization;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new KittyApp();
-
-//$app->get('/dna/body/{code}', [DnaController::class, 'searchBody']);
-//$app->get('/dna/body/color/{code}', [DnaController::class, 'searchBodyColor']);
-//
-//$app->get('/dna/pattern/{code}', [DnaController::class, 'searchPattern']);
-//$app->get('/dna/pattern/color/{code}', [DnaController::class, 'searchPatternColor']);
-//$app->get('/dna/eye/color/{code}', [DnaController::class, 'searchEyeColor']);
-//$app->get('/dna/eye/type/{code}', [DnaController::class, 'searchEyeType']);
-//
-//$app->get('/dna/secondary/color/{code}', [DnaController::class, 'searchSecondaryColor']);
-//
-//$app->get('/dna/wild/{code}', [DnaController::class, 'searchWild']);
-//
-//$app->get('/dna/mouth/{code}', [DnaController::class, 'searchMouth']);
-//
-//$app->get('/dna/kitty/{kittenId}', [DnaController::class, 'searchForKitten']);
-//$app->get('/dna/kitty/{kittenId}/pretty', [DnaController::class, 'searchForKittenPretty']);
-
-//$app->get('/dna/profile/{profile}', ProfileController::class);
-
-//Old Routes
-$app->get('/profile', function (Request $request, Response $response) {
-    return $response->withRedirect('http://cryptokittydata.info');
-});
-
-$app->get('/', function (Request $request, Response $response) {
-    return $response->withRedirect('http://cryptokittydata.info');
-});
-
-
-
 
 //Fucking CORS
 $app->options('{name:.+}', CorsBullshit::class);
