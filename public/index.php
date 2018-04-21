bdc2bff42855ebc73c752d9ff7bb726307b3c30c
Glenn Eggeton <geggleton@mobials.com>
2018-04-21 01:15:11 -0400

    Update

--- public/index.php
+++ public/index.php
@@ -40,7 +40,7 @@ $app->add(function (Request $req, Response $res, $next) {
     $response = $next($req, $res);
 
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://cryptokittydata.info')
+        ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
--- src/Http/CorsBullshit.php
+++ src/Http/CorsBullshit.php
@@ -11,8 +11,6 @@ class CorsBullshit
 {
     public function __invoke(Request $request, Response $response)
     {
-        return $response->withHeader('Access-Control-Allow-Origin', '*')
-                        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-                        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        return $response;
     }
 }
