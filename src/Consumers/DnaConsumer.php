b7d744e8e716f109fc788b867604251b34626123
Glenn Eggeton <geggleton@mobials.com>
2018-06-13 23:18:09 -0400

    Update

--- src/Consumers/DnaConsumer.php
+++ src/Consumers/DnaConsumer.php
@@ -5,6 +5,7 @@ namespace Kitty\Consumers;
 
 
 use Kitty\Services\KittyService;
+use Monolog\Logger;
 
 class DnaConsumer implements ConsumerInterface
 {
@@ -16,11 +17,22 @@ class DnaConsumer implements ConsumerInterface
      * @var \PDO
      */
     private $PDO;
+    /**
+     * @var Logger
+     */
+    private $logger;
 
-    public function __construct(\PDO $PDO)
+    /**
+     * DnaConsumer constructor.
+     *
+     * @param \PDO   $PDO
+     * @param Logger $logger
+     */
+    public function __construct(\PDO $PDO, Logger $logger)
     {
         $this->PDO = $PDO;
         $this->statement = $this->PDO->prepare('update kitties set genes_hex = ?, genes_bin = ?, genes_kai =? where id = ?');
+        $this->logger = $logger;
     }
 
     public function __invoke(array $args)
@@ -31,6 +43,8 @@ class DnaConsumer implements ConsumerInterface
 
             return $this->statement->execute([$dna['hex'], $dna['bin'], $dna['kai'], $id]);
         } catch (\Exception $exception) {
+            $this->logger->error($exception->getMessage());
+
             return false;
         }
     }
